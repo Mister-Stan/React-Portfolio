1a13c3a2ba5371700068dda2160a78a9a7427456
Eugeniu Stan <lucianstan87@yahoo.com>
2023-03-09 23:54:14 +0000

    adds the homepage.js file

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageDisplay from '../components/PageDisplay.jsx';
+
+const Homepage = () => {
+  return (
+      <PageDisplay>
+      <h1>homepage</h1>
+      </PageDisplay>
+  )
+}
+
+export default Homepage
