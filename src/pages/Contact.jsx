a04c679c7a5baa1983c298c1a6fe2defb4fc8f43
Eugeniu Stan <lucianstan87@yahoo.com>
2023-03-09 23:59:41 +0000

    adds the Contact.js file

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageDisplay from '../components/PageDisplay.jsx';
+
+const Contact = () => {
+  return (
+      <PageDisplay>
+      <h1>contact</h1>
+      </PageDisplay>
+  )
+}
+
+export default Contact
