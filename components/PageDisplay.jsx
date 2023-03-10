26d992176b362a72e05f20486977b8a825e89579
Eugeniu Stan <lucianstan87@yahoo.com>
2023-03-10 00:07:51 +0000

    adds the pageDisplay.js file

---
+++ components/PageDisplay.jsx
@@ -0,0 +1,18 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+import React from 'react'
+
+const PageDisplay = (props) => {
+  return (
+    <div className='container-fluid'>
+      <Header />
+      {/* <h1> PageDisplay</h1> */}
+      {props.children}
+      <Footer />
+    </div>
+  )
+}
+
+export default PageDisplay
+
