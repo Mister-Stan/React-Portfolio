42377888a8db2bf9ca1cd2000559fd3c0e37d9af
mister-stan <lucianstan87@yahoo.com>
2023-03-13 00:28:59 +0000

    Update Footer.jsx
    Adds some extra style to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container" style={{ marginTop: "20rem"}}>
+    <div className="container" style={{ marginTop: "20rem", marginBottom:"2rem"}}>
      
       <h5 style={{ display: "inline-block", marginRight: "9rem" }}>
         Email:{" "}
