0bdeb6493ba7ab5b82eb09afeb8b4e861e231715
mister-stan <lucianstan87@yahoo.com>
2023-04-03 17:34:11 +0100

    Update Contact.jsx
    make cv images mobile responseive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,29 +3,30 @@ import Form from '../components/Form';
 
 const Contact = () => {
   return (
-      <section className="container" style={{
-  boxShadow: "10px 10px 15px 15px grey",
-  padding: "2rem",
-  borderRadius: "10px",
-}}>
-        <h4>
+    <section className="container" style={{
+      boxShadow: "10px 10px 15px 15px grey",
+      padding: "2rem",
+      borderRadius: "10px",
+    }}>
+      <h4>
         Let's link up like Wi-Fi! Drop me a message using this form to share your thoughts:
-        </h4> 
-        <div>
-          <Form />
-          <div style={{marginTop:"50px"}}>
-        <h4>My CV:</h4>
-        <div style={{display:"flex", marginLeft:"7%", marginBottom:"30px"}}>
-        <img src='./images/cv-first.png' alt="myCVfirstPage"></img>
-        <img src='./images/cv-second.png' alt="myCVSecondPage"></img>
+      </h4> 
+      <div>
+        <Form />
+        <div style={{marginTop:"50px"}}>
+          <h4>My CV:</h4>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div style={{margin: 'auto', maxWidth: '100%'}}>
+              <img src='./images/cv-first.png' alt="myCVfirstPage" style={{width: '100%'}}></img>
+            </div>
+            <div style={{margin: 'auto', maxWidth: '100%'}}>
+              <img src='./images/cv-second.png' alt="myCVSecondPage" style={{width: '100%'}}></img>
+            </div>
+          </div>
+          <a href="./cv/EugeniuStan.pdf" download>Download my CV</a>
         </div>
-        <a href="./cv/EugeniuStan.pdf" download>Download my CV</a>
-        
-        </div>
-        </div>
-      </section>
-      
-      
+      </div>
+    </section>
   );
 };
 
