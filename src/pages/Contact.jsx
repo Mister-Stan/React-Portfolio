6f94a40e28b23f6a2ae10868396f335883a1f4a4
mister-stan <lucianstan87@yahoo.com>
2023-04-03 15:45:59 +0100

    contact page
    add extra styling to the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,13 +8,13 @@ const Contact = () => {
   padding: "2rem",
   borderRadius: "10px",
 }}>
-        <h2>
+        <h4>
         Let's link up like Wi-Fi! Drop me a message using this form to share your thoughts:
-        </h2> 
+        </h4> 
         <div>
           <Form />
           <div style={{marginTop:"50px"}}>
-        <h3>My CV:</h3>
+        <h4>My CV:</h4>
         <div style={{display:"flex", marginLeft:"7%", marginBottom:"30px"}}>
         <img src='./images/cv-first.png' alt="myCVfirstPage"></img>
         <img src='./images/cv-second.png' alt="myCVSecondPage"></img>
